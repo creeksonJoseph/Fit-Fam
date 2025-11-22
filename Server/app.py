ece475a5ef24312190ca35c3a9a4452f85448840
creeksonjoseph <charanajoseph@gmail.com>
2025-11-22 13:20:01 +0300

    changed cors config

--- Server/app.py
+++ Server/app.py
@@ -34,13 +34,7 @@ def create_app():
     Session(app)
 
     CORS(app, 
-         origins=[
-             "http://localhost:3000",
-             "http://127.0.0.1:3000",
-             'https://fit-fam.onrender.com',
-             'https://group-fitness-app.onrender.com',
-             'https://fit-fam-eight.vercel.app',
-             'https://fit-fam-six.vercel.app'], 
+         origins=['https://fit-fam-1.vercel.app'], 
          supports_credentials=True, 
          allow_headers=['Content-Type', 'Authorization'],
          methods=['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'])
